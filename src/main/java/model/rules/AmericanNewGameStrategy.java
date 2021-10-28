da55c5250ef4044d538e9c342905bda68f940788
Emma Lovgren <el222wg@student.lnu.se>
2021-10-28 23:45:48 +0200

    Added AmericanNewGameStrategy

--- src/main/java/model/rules/AmericanNewGameStrategy.java
+++ src/main/java/model/rules/AmericanNewGameStrategy.java
@@ -7,25 +7,51 @@ import model.Player;
 
 class AmericanNewGameStrategy implements NewGameStrategy {
 
+  private int numberOfCardsToDraw; 
+
+  /**
+   * Initializing constructor.
+   */
+  public AmericanNewGameStrategy() {
+    this.numberOfCardsToDraw = 4;
+  }
+
+  /**
+   * Start game with four cards, alternating between giving card to dealer or player.
+   * The last card is not shown to the players.
+   */
   public boolean newGame(Deck deck, Dealer dealer, Player player) {
-    Card.Mutable c;
+    // Card.Mutable c;
 
-    c = deck.getCard();
-    c.show(true);
-    player.dealCard(c);
+    // c = deck.getCard();
+    // c.show(true);
+    // player.dealCard(c);
 
-    c = deck.getCard();
-    c.show(true);
-    dealer.dealCard(c);
+    // c = deck.getCard();
+    // c.show(true);
+    // dealer.dealCard(c);
+    
+    // c = deck.getCard();
+    // c.show(true);
+    // player.dealCard(c);
 
-    c = deck.getCard();
-    c.show(true);
-    player.dealCard(c);
+    // c = deck.getCard();
+    // c.show(false);
+    // dealer.dealCard(c);
 
-    c = deck.getCard();
-    c.show(false);
-    dealer.dealCard(c);
+    for (int i = 0; i < this.numberOfCardsToDraw; i++) {
+      boolean showCard = true;
 
+      if (i == (this.numberOfCardsToDraw - 1)) {
+        showCard = false;
+      }
+
+      if (i % 2 == 0) {
+        deck.drawCards(dealer, showCard);  
+      } else {
+        deck.drawCards(player, showCard);  
+      }              
+    }
     return true;
   }
 }
