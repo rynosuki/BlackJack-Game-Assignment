861aad5a3698151572c0111179bb50df38e641ee
Emma Lovgren <el222wg@student.lnu.se>
2021-10-28 23:56:29 +0200

    Added InternationalNewGameStrategy

--- src/main/java/model/rules/InternationalNewGameStrategy.java
+++ src/main/java/model/rules/InternationalNewGameStrategy.java
@@ -8,20 +8,38 @@ import model.Player;
 
 class InternationalNewGameStrategy implements NewGameStrategy {
 
+  private int numberOfCardsToDraw; 
+
+  /**
+   * Initializing constructor.
+   */
+  public InternationalNewGameStrategy() {
+    this.numberOfCardsToDraw = 3;
+  }
+
   public boolean newGame(Deck deck, Dealer dealer, Player player) {
-    Card.Mutable c;
+    // Card.Mutable c;
 
-    c = deck.getCard();
-    c.show(true);
-    player.dealCard(c);
+    // c = deck.getCard();
+    // c.show(true);
+    // player.dealCard(c);
 
-    c = deck.getCard();
-    c.show(true);
-    dealer.dealCard(c);
+    // c = deck.getCard();
+    // c.show(true);
+    // dealer.dealCard(c);
 
-    c = deck.getCard();
-    c.show(true);
-    player.dealCard(c);
+    // c = deck.getCard();
+    // c.show(true);
+    // player.dealCard(c);
+
+    for (int i = 0; i < numberOfCardsToDraw; i++) {
+      
+      if (i % 2 == 0) {
+        deck.drawCards(dealer, true);  
+      } else {
+        deck.drawCards(player, true);  
+      }            
+    }
 
     return true;
   }
