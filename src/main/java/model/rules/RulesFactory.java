df8748bf9cde0774e18c71b81cfd0e889bce2ab0
Robin Svensson <rs223dj@student.lnu.se>
2021-10-29 14:04:41 +0200

    Small fix

--- src/main/java/model/rules/RulesFactory.java
+++ src/main/java/model/rules/RulesFactory.java
@@ -7,16 +7,16 @@ public class RulesFactory {
 
   /**
    * Creates the rule to use for the dealer's hit behavior.
-
+   * 
    * @return The rule to use
    */
   public HitStrategy getHitRule() {
-    return new BasicHitStrategy();
+    return new Soft17();
   }
 
   /**
    * Crates the rule to use when starting a new game.
-
+   * 
    * @return The rule to use.
    */
   public NewGameStrategy getNewGameRule() {
