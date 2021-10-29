831928d55ac11abf8b3315d36785590aadd92441
Robin Svensson <rs223dj@student.lnu.se>
2021-10-29 14:49:26 +0200

    Checkstyle

--- src/main/java/model/rules/PlayerWins.java
+++ src/main/java/model/rules/PlayerWins.java
@@ -1,12 +1,11 @@
 package model.rules;
 
-import model.Card;
 import model.Player;
 
 class PlayerWins implements WinStrategy {
-  
+
   public boolean doWin(Player dealer, Player player) {
-    
+
     return dealer.calcScore() > player.calcScore();
   }
 }
